b310bf208165470b939d9b40219344ec3c97323d
Saharsh Samir <saharshsamir10@gmail.com>
2022-12-27 22:22:16 +0530

    problem with git auth

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,23 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust", "help", "javascript", "typescript" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+
+    additional_vim_regex_highlighting = false,
+  },
+}
--- lua/saharsh/plug.lua
+++ lua/saharsh/plug.lua
@@ -4,5 +4,32 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 	Plug('nvim-lua/plenary.nvim')
 	Plug('nvim-telescope/telescope.nvim', {tag = '0.1.0'})
+	Plug('folke/tokyonight.nvim', { branch = 'main' })
+	Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+	--LSP Support
+	Plug('neovim/nvim-lspconfig')
+	Plug('williamboman/mason.nvim')
+	Plug('williamboman/mason-lspconfig.nvim')
+
+	--Autocompletion
+	Plug('hrsh7th/nvim-cmp')
+	Plug('hrsh7th/cmp-buffer')
+	Plug('hrsh7th/cmp-path')
+	Plug('saadparwaiz1/cmp_luasnip')
+	Plug('hrsh7th/cmp-nvim-lsp')
+	Plug('hrsh7th/cmp-nvim-lua')
+
+	--Snippetsjhhh
+	Plug('L3MON4D3/LuaSnip')
+	Plug('rafamadriz/friendly-snippets')
+
+	Plug('VonHeikemen/lsp-zero.nvim')
+
+	Plug('ray-x/lsp_signature.nvim')
+	Plug('kyazdani42/nvim-web-devicons')
+	--Plug('onsails/lspkind-nvim')
+	Plug("ellisonleao/gruvbox.nvim")
+	
 
 vim.call('plug#end')
